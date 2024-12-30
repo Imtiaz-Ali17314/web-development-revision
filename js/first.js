6f3dc2fb14e437598ac6f2d6f0c806a298d99813
Imtiaz Ali <imtiazali17314@gmail.com>
2024-12-30 22:13:00 +0500

    Lecture 3 loops paractic Q1 even and odd numbers.

--- js/first.js
+++ js/first.js
@@ -1,20 +1,13 @@
-// If-else Statement
-// Let's Paractice Q no.2
+// Loops
+// Lect 3  Paractice Q 3.
 
-let score = prompt("Enter your score");
-
-if (score >= 90 && score <= 100) {
-  console.log("A");
-} else if (score >= 80 && score <= 89) {
-  console.log("B");
-} else if (score >= 70 && score <= 79) {
-  console.log("C");
-} else if (score >= 60 && score <= 69) {
-  console.log("D");
-} else if (score >= 50 && score <= 59) {
-  console.log("E");
-} else if (score >= 0 && score <= 49) {
-  console.log("F");
-} else {
-  console.log("Invalid Score");
+for (let i = 1; i <= 100; i++) {
+  // even numbers
+  if (i % 2 == 0) {
+    console.log("even num :", i);
+  }
+  // odd numbers
+  else {
+    console.log("odd num :", i);
+  }
 }
