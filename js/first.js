87633f7c63630e32a5558f425b725a98622ca35a
Imtiaz Ali <imtiazali17314@gmail.com>
2024-12-30 19:32:20 +0500

    Lect. 2 Paractice Q.2

--- js/first.js
+++ js/first.js
@@ -1,17 +1,20 @@
-// Logical Operators
-// Logical AND &&        Logical OR ||         Logical NOT !
+// If-else Statement
+// Let's Paractice Q no.2
 
-let a = 12;
-let b = 5;
+let score = prompt("Enter your score");
 
-console.log("12 > 5 && b = 5", a > b && b === 5); // True
-
-console.log("12 > 5 && b = 6", a > b && b === 6); // False
-
-console.log("12 > 5 || b = 6", a > b || b === 6); // True
-
-console.log("12 = 5 || b = 6", a == b && b === 6); // False
-
-console.log("!( a = b )", !(a == b)); // True
-
-console.log("!( a > b )", !(a > b)); // False
+if (score >= 90 && score <= 100) {
+  console.log("A");
+} else if (score >= 80 && score <= 89) {
+  console.log("B");
+} else if (score >= 70 && score <= 79) {
+  console.log("C");
+} else if (score >= 60 && score <= 69) {
+  console.log("D");
+} else if (score >= 50 && score <= 59) {
+  console.log("E");
+} else if (score >= 0 && score <= 49) {
+  console.log("F");
+} else {
+  console.log("Invalid Score");
+}
