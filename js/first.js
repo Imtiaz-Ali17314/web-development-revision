b97036837b0a9887beaa9b02b58c239ac59eb67f
Imtiaz Ali <imtiazali17314@gmail.com>
2024-12-30 09:05:03 +0500

    Assignment operators

--- js/first.js
+++ js/first.js
@@ -1,15 +1,23 @@
+// Assignment Operators
+// = , += , -= , *= , %= , **=
+
 let a = 12;
-
 let b = 5;
+let c = 8;
+let d = 13;
+let e = 2;
 
-console.log("a+b = ", a + b);
-console.log("a-b = ", a - b);
-console.log("a*b = ", a * b);
-console.log("a/b = ", a / b);
-console.log("a%b = ", a % b);
+// a = a+2    Addition
+console.log("a = ", (a += 3));
 
-// Pree Increment
-console.log("++a = ", ++a);
+// b = b-2    Subtraction
+console.log("b = ", (b -= 2));
 
-// Post Increment
-// console.log("a++ = ", a++);
+// c = c*2    Multiplication
+console.log("c = ", (c *= 2));
+
+// d = d%2    module
+console.log("d = ", (d %= 2));
+
+// e = e**2   Power
+console.log("e = ", (e **= 2));
