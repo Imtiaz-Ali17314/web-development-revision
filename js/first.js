ea9d9e55856d6a6ead18d92dcf291d80fd03a839
Imtiaz Ali <imtiazali17314@gmail.com>
2024-12-31 15:48:53 +0500

    lecture 4 Arrays prarctice Q2

--- js/first.js
+++ js/first.js
@@ -1,16 +1,12 @@
 // Arrays
 
-// Paractice Q1
+// Paractice Q2. For a given array with prices of 5 items -> [250, 645, 300, 900, 50] All items have an offer of 10% OFF on them. Change the array to store final price after
+// applying offer.
 
-let marks = [85, 97, 44, 37, 76, 60];
-let sum = 0;
+let items = [250, 645, 300, 900, 50];
 
-for (val of marks) {
-  sum += val;
+for (let i = 0; i < items.length; i++) {
+  let offer = items[i] / 10;
+  items[i] = items[i] - offer;
 }
-
-console.log(sum);
-console.log(marks.length);
-let avg = sum / marks.length;
-
-console.log(`The average marks of class = ${avg}`);
+console.log(items);
