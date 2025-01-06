252cbc73c68278bc1d38f48a5c1df3fa9d24c072
Imtiaz Ali <imtiazali17314@gmail.com>
2025-01-06 10:35:11 +0500

    Lecture 5 Functions and Methods complete Practice Q's

--- js/first.js
+++ js/first.js
@@ -1,15 +1,34 @@
-// Functions Methods ===>  Reduce function in Array
-// Reduce function perform some operations and reduce the array into a single value. it retruns a single value.
+// Functions        ==>  Array Methods
+// Practice Q1
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8];
-console.log(arr);
+let marks = [45, 67, 90, 89, 96, 99, 45, 97, 84, 91];
+console.log(marks);
 
-let largestNo = arr.reduce((prev, curr) => {
-  return prev > curr ? prev : curr;
+const topMarks = marks.filter((val) => {
+  return val >= 90;
 });
-console.log("Largest number in the array ", largestNo);
+console.log(topMarks);
 
-let sumArr = arr.reduce((prev, curr) => {
+// Practice Q2
+
+let n = prompt("Enter a number: ");
+
+let arr = [];
+for (i = 1; i <= n; i++) {
+  arr[i - 1] = i;
+}
+console.log("User array: ", arr);
+
+// sum of all numbers in the array.
+
+let sumOfArrar = arr.reduce((prev, curr) => {
   return prev + curr;
 });
-console.log("Sum of all numbers in the array ", sumArr);
+console.log("Sum of all numbers in the array: ", sumOfArrar);
+
+// Product of all numbers(Factorial) in the array.
+
+let factorial = arr.reduce((prev, curr) => {
+  return prev * curr;
+});
+console.log("Factorial: ", factorial);
