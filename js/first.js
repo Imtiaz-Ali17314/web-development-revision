911d91d240b8efc9624cdcb403a6f3c72de9b6df
Imtiaz Ali <imtiazali17314@gmail.com>
2025-01-06 20:34:45 +0500

    Lecture 6 DOM Document Object Model, Selectors and properties of DOM. Practice 1 Q.1

--- js/first.js
+++ js/first.js
@@ -1,34 +1,4 @@
-// Functions        ==>  Array Methods
-// Practice Q1
+let h2 = document.querySelector("h2");
+console.dir(h2.innerText);
 
-let marks = [45, 67, 90, 89, 96, 99, 45, 97, 84, 91];
-console.log(marks);
-
-const topMarks = marks.filter((val) => {
-  return val >= 90;
-});
-console.log(topMarks);
-
-// Practice Q2
-
-let n = prompt("Enter a number: ");
-
-let arr = [];
-for (i = 1; i <= n; i++) {
-  arr[i - 1] = i;
-}
-console.log("User array: ", arr);
-
-// sum of all numbers in the array.
-
-let sumOfArrar = arr.reduce((prev, curr) => {
-  return prev + curr;
-});
-console.log("Sum of all numbers in the array: ", sumOfArrar);
-
-// Product of all numbers(Factorial) in the array.
-
-let factorial = arr.reduce((prev, curr) => {
-  return prev * curr;
-});
-console.log("Factorial: ", factorial);
+h2.innerText = h2.innerText + " from apnaCollege students";
