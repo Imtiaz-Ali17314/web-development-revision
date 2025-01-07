a3c8955b36baa4ca9b7f7fd8cd4bd48e10981518
Imtiaz Ali <imtiazali17314@gmail.com>
2025-01-07 22:03:08 +0500

    Lect 7 DOM complete with lets practice Q's

--- js/first.js
+++ js/first.js
@@ -1,22 +1,9 @@
-let mainHeading = document.createElement("h1"); // Create h1 heading at the top
-mainHeading.innerText = "Hello! This is Java Script";
-let body = document.querySelector("body");
-body.prepend(mainHeading);
+let button = document.createElement("button");
+button.innerText = "Click me!";
+button.style.backgroundColor = "red";
+button.style.color = "white";
 
-let itemList = document.createElement("p"); // Create paragraph in the div at top
-itemList.innerText = "item list";
-let ul = document.querySelector("ul");
-ul.before(itemList);
+document.querySelector("body").prepend(button);
 
-let listBtn = document.createElement("button"); // Create button in div at bottom
-listBtn.innerText = "Click me!";
-document.querySelector("div").append(listBtn);
-
-let btn = document.createElement("button"); // Create button at page bottom
-btn.innerText = "Submit";
-document.querySelector(".para").after(btn);
-
-document.querySelector(".toRemove").remove(); // delete div at after listItem div
-
-let div = document.querySelector("div"); // change height of div
-div.style.height = "150px";
+let p = document.querySelector("p");
+p.classList.add("newClass");
