c78ed334388d3c2e9c7964784a4490fc5ca69c44
Imtiaz Ali <imtiazali17314@gmail.com>
2025-01-07 19:49:09 +0500

    DOM , Access html attributes and change them and also change its styling (css)

--- js/first.js
+++ js/first.js
@@ -1,12 +1,32 @@
-let divs = document.querySelectorAll(".box");
-console.log(divs);
+let div = document.querySelector("div"); // Access node --> div
+console.log(div);
 
-// div[0].innerText = "Unique value 1";
-// div[1].innerText = "Unique value 2";
-// div[2].innerText = "Unique value 3";
+let id = div.getAttribute("id"); // Access id attribute value
+console.log("Old Id: ", id);
 
-let indx = 1;
-for (div of divs) {
-  div.innerText = `new unique value is ${indx}`;
-  indx++;
-}
+div.setAttribute("id", "div1"); // Change id attribute value
+id = div.getAttribute("id");
+console.log("new Id: ", id);
+
+let name = div.getAttribute("name"); // Access name attribute value
+console.log("Old name: ", name);
+
+div.setAttribute("name", "firstDiv"); // Change name attribute value
+name = div.getAttribute("name");
+console.log("new name: ", name);
+
+let p = document.querySelector("p"); // Access node --> p
+console.log(p);
+
+let clas = p.getAttribute("class"); // Access class attribute value
+console.log("old class: ", clas);
+
+p.setAttribute("class", "paragraph"); // Change class attribute value
+clas = p.getAttribute("class");
+console.log("new class: ", clas);
+
+p.style.color = "red"; // Change style with js
+p.style.fontSize = "20px";
+p.style.fontStyle = "italic";
+div.style.backgroundColor = "green";
+div.style.color = "white";
