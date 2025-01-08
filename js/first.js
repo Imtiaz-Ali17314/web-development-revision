faf4530c9676497c0d971009cb0c9c95b825ac4d
Imtiaz Ali <imtiazali17314@gmail.com>
2025-01-08 08:31:33 +0500

    Lect 8 complete with practice Q's. create toggle button.

--- js/first.js
+++ js/first.js
@@ -1,9 +1,31 @@
-let button = document.createElement("button");
-button.innerText = "Click me!";
-button.style.backgroundColor = "red";
-button.style.color = "white";
+let div = document.querySelector("div");
 
-document.querySelector("body").prepend(button);
+let changeColor = (e) => {
+  console.log(e.target);
+  console.log(e.type);
+  console.log(e.clientX);
+  console.log(e.clientY);
+};
+div.addEventListener("mouseover", changeColor);
 
-let p = document.querySelector("p");
-p.classList.add("newClass");
+let body = document.querySelector("body");
+let bodyClass = body.getAttribute("class");
+
+let button = document.querySelector("button");
+let currMode = "light";
+
+button.addEventListener("click", (e) => {
+  if (currMode == "light") {
+    currMode = "dark";
+    console.log(currMode);
+
+    body.classList.add("dark");
+    body.classList.remove("light");
+  } else {
+    currMode = "light";
+    console.log(currMode);
+
+    body.classList.add("light");
+    body.classList.remove("dark");
+  }
+});
