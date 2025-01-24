5d5ee30810137730ac0af3bbfdf5233c69a532b5
Imtiaz Ali <imtiazali17314@gmail.com>
2025-01-24 19:59:32 +0500

    fetch api

--- js/first.js
+++ js/first.js
@@ -1,113 +1,17 @@
-// function api() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log("weather data");
-//       resolve(200);
-//     }, 2000);
-//   });
-// }
+const URL = "https://dog.ceo/api/breeds/list/all";
+const para = document.querySelector("#table");
 
-// async function getWeatherData(params) {
-//   await api();
-//   await api();
-// }
+let dogData = [];
+const getDogPics = async () => {
+  console.log("getting data .... ");
+  let response = await fetch(URL);
+  console.log(response);
+  let data = await response.json();
+  dogData = data.message.terrier;
+  for (let data of dogData) {
+    para.innerHTML =
+      para.innerHTML + "<ul><li><tr><td>" + data + "</td></tr></li></ul>";
+  }
+};
 
-// function asyncFunc1() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log("Data1");
-//       resolve("success");
-//     }, 2000);
-//   });
-// }
-
-// function asyncFunc2() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log("Data2");
-//       resolve("success");
-//     }, 2000);
-//   });
-// }
-// //  promise Chain
-// console.log("fetching data1 .....");
-// asyncFunc1().then((res) => {
-//   console.log("fetching data2  .....");
-//   asyncFunc2().then((res) => {});
-// });
-
-// const getNewPromise = () => {
-//   return new Promise((resolve, reject) => {
-//     console.log("hy! I am a promise");
-//     resolve("successfully");
-//     // reject("network error");
-//   });
-// };
-
-// let promise = getNewPromise();
-// // if promise resolve
-// promise.then((res) => {
-//   console.log("fulFilled", res);
-// });
-// // if promise reject
-// promise.catch((err) => {
-//   console.log("rejected", err);
-// });
-
-function getData(idData, getNextData) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("Data", idData);
-      resolve("successfully get data");
-      if (getNextData) {
-        getNextData();
-      }
-    }, 2000);
-  });
-}
-
-// Async await
-
-// async function getAllData() {
-//   console.log("fetching data1 ......");
-//   await getData(1);
-//   console.log("fetching data2 ......");
-//   await getData(2);
-//   console.log("fetching data3 ......");
-//   await getData(3);
-// }
-
-// IIFE
-
-(async function getAllData() {
-  console.log("fetching data1 ......");
-  await getData(1);
-  console.log("fetching data2 ......");
-  await getData(2);
-  console.log("fetching data3 ......");
-  await getData(3);
-})();
-
-// // Promise Chain
-// console.log("fetching data 1");
-// getData(1)
-//   .then((res) => {
-//     console.log("fetching data 2");
-//     return getData(2);
-//   })
-//   .then((res) => {
-//     console.log("fetching data 3");
-//     return getData(3);
-//   })
-//   .then((res) => {
-//     console.log("fetching data 4");
-//     return getData(4);
-//   });
-
-// //  Callback hell
-
-// getData(1, () => {
-//   getData(2, () => {
-//     getData(3);
-//   });
-// });
+getDogPics();
